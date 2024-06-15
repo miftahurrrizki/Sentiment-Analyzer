2a337ec791f2b6b22cd2fc8595a7700d00449b5c
Zado <zado.keren@gmail.com>
2024-06-15 18:19:55 +0700

    psuiing

--- script/script.js
+++ script/script.js
@@ -5,10 +5,11 @@ function showOutput() {
         return;
     }
 
+    const corsProxy = 'https://cors-anywhere.herokuapp.com/';
     const apiUrl = 'http://57.155.53.48/predict/';
 
     $.ajax({
-        url: apiUrl,
+        url: corsProxy + apiUrl,
         type: 'POST',
         contentType: 'application/json',
         data: JSON.stringify({
